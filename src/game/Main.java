9636eaf5c3bb51b04a259f25d0b5c8efe7ad9bb3
minhlqd <48119305+minhlqd@users.noreply.github.com>
2019-10-26 14:22:31 +0700

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,32 +1,13 @@
 package game;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.effect.Glow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.*;
-import javafx.util.Duration;
-
-//import javafx.swing.*;
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 public class Main extends Application {
 
